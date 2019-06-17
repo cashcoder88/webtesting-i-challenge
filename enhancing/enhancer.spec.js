744b889ea2b63de797633614312bb1502e8a959b
cashcoder88 <cashglobe@gmail.com>
2019-06-17 17:16:40 -0600

    first portion of succeed test working

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,10 +1,17 @@
-const {repair} = require('./enhancer.js');
+const {repair, succeed} = require('./enhancer.js');
 // test away!
 describe('enhancer.js', () => {
     describe('repair()', () => {
         it('repairs durability to 100', () => {
             expect(repair({durability: 75}).durability).toBe(100);
             expect(repair({durability: -5}).durability).toBe(100);
+            expect(repair({durability: 101}).durability).toBe(100);
+        });
+    });
+
+    describe('succeed()', () => {
+        it('increases items enhancement + 1', () => {
+            expect(succeed({enhancement: 5}).enhancement).toBe(6);
         });
     });
 });
